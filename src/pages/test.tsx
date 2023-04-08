7a1b662ec7121f5ba6bb672f6445a67831ac0c76
Chan Che Wei <chanchewei@moneymatch.co>
2023-04-08 22:56:51 +0800

    Added CVR template

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,7 +5,7 @@ const Test: React.FC = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 bg-gray-100 p-10">
+      <div className="flex-1 overflow-auto bg-gray-100 p-10">
         <div className="mb-6 flex">
           <div className="ml-auto">
             <button className="rounded-md bg-[#538DFF] px-5 py-2 text-white">
@@ -15,7 +15,7 @@ const Test: React.FC = () => {
         </div>
         <div className="flex gap-8">
           <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner">
-            <div className="flex h-full flex-col p-5 items-center justify-center">
+            <div className="flex h-full flex-col items-center justify-center p-5">
               <div className="mb-4">
                 <Image
                   src="/contract-icon.png"
@@ -26,11 +26,14 @@ const Test: React.FC = () => {
               </div>
               <>
                 <h2 className="text-xl font-bold">Contract Value</h2>
-                <p className="text-3xl text-[#0075FF] font-bold">RM10 million</p>
+                <p className="text-3xl font-bold text-[#0075FF]">
+                  RM10 million
+                </p>
               </>
             </div>
           </div>
-          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner"><div className="flex h-full flex-col p-5 items-center justify-center">
+          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner">
+            <div className="flex h-full flex-col items-center justify-center p-5">
               <div className="mb-4">
                 <Image
                   src="/tender-cost-icon.png"
@@ -41,11 +44,13 @@ const Test: React.FC = () => {
               </div>
               <>
                 <h2 className="text-xl font-bold">Tender Costs</h2>
-                <p className="text-3xl text-[#0075FF] font-bold">RM8 million</p>
+                <p className="text-3xl font-bold text-[#0075FF]">RM8 million</p>
               </>
-            </div></div>
-          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner"><div className="flex h-full flex-col p-5 items-center justify-center">
-              <div className="mb-4 flex gap-3 items-center">
+            </div>
+          </div>
+          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner">
+            <div className="flex h-full flex-col items-center justify-center p-5">
+              <div className="mb-4 flex items-center gap-3">
                 <Image
                   src="/tender-margin-icon.png"
                   alt="Icon"
@@ -56,10 +61,12 @@ const Test: React.FC = () => {
               </div>
               <>
                 <h2 className="text-xl font-bold">Tender Margin</h2>
-                <p className="text-3xl text-[#0075FF] font-bold">RM2 million</p>
+                <p className="text-3xl font-bold text-[#0075FF]">RM2 million</p>
               </>
-            </div></div>
-          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner"><div className="flex h-full flex-col p-5 items-center justify-center">
+            </div>
+          </div>
+          <div className="aspect-square flex-1 rounded-[10%] bg-white shadow-lg hover:shadow-inner">
+            <div className="flex h-full flex-col items-center justify-center p-5">
               <div className="mb-4">
                 <Image
                   src="/duration-icon.png"
@@ -70,9 +77,35 @@ const Test: React.FC = () => {
               </div>
               <>
                 <h2 className="text-xl font-bold">Project Duration</h2>
-                <p className="text-3xl text-[#0075FF] font-bold">10 months</p>
+                <p className="text-3xl font-bold text-[#0075FF]">10 months</p>
               </>
-            </div></div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 flex gap-3">
+          <div className=" flex flex-[1.5] flex-col gap-3">
+            <div className="flex flex-[1.5] flex-col items-center justify-center rounded-[25px] bg-white text-center shadow-lg hover:shadow-inner">
+              <h2 className="text-xl">Actual Margin</h2>
+              <h1 className="my-3 text-6xl">30%</h1>
+              <p className="text-xl text-gray-400">
+                (True Value - True Cost) / True Value
+              </p>
+            </div>
+            <div className=" flex flex-[2] flex-col justify-center pl-5 text-2xl font-medium leading-[3rem] text-gray-400">
+              <h2>General Explaination:</h2>
+              <h2>True Value (work done on site) = 10</h2>
+              <h2>True Costs = 8</h2>
+              <h2>Actual Margin = 2</h2>
+            </div>
+          </div>
+          <div className="flex flex-[2] flex-col gap-3">
+            <div className="aspect-video flex-1 rounded-[25px] bg-white shadow-lg">
+              Marginal Status
+            </div>
+            <div className="aspect-video flex-1 rounded-[25px] bg-white shadow-lg">
+              True Value vs True Cost (Accumulated)
+            </div>
+          </div>
         </div>
       </div>
     </div>
